4f7859e179fc6151cdcb35a76820170df79b1d6c
alejandru00 <alejandrolliber2004@gmail.com>
2023-03-17 12:20:48 +0100

    cambios en barco

--- clases/Barco.py
+++ clases/Barco.py
@@ -2,55 +2,57 @@
 from itertools import repeat, product
 from random import choice
 from typing import Self
-from juego import HORIZONTAL, VERTICAL, ORIENTACIONES, Barco, Case, Conventions
+from juego import HORIZONTAL, LONGITUDES_BARCOS, VERTICAL, ORIENTACIONES, Barco, Case, Conventions
 from decimal import Decimal, getcontext, ConversionSyntax, ROUND_HALF_UP, ROUND_HALF_EVEN, ROUND_HALF_DOWN, ROUND_UP, ROUND_DOWN, ROUND_CEILING, ROUND_FLOOR, ROUND_05UP
 
 instances = []
 casillas_ocupadas = set()
 
-def __init__(self, longitud):
+# performance / legibilidad:
+num_lineas = Conventions.tablero_num_lineas
+num_columnas = Conventions.tablero_num_columnas
+num2l = Conventions.generar_num_linea
+num2c = Conventions.generar_num_columna
+
+def __init__(self, longitud, orientacion, tocado, hundido):
         self.longitud = longitud
         self.orientacion = choice(ORIENTACIONES)
         self.tocado = False
         self.hundido = False
 
-        # performance / legibilidad:
-        num_lineas = Conventions.tablero_num_lineas
-        num_columnas = Conventions.tablero_num_columnas
-        num2l = Conventions.generar_num_linea
-        num2c = Conventions.generar_num_columna
-
-        while True:
+def horizontal(self):  
             if self.orientacion == HORIZONTAL:
                 rang = choice(range(num_lineas))
-                primero = choice(range(num_columnas + 1 - longitud))
+                primero = choice(range(num_columnas + 1 - LONGITUDES_BARCOS))
                 letra = num2l(rang)
-                cifras = [num2c(x) for x in range(primero, primero + longitud)]
+                cifras = [num2c(x) for x in range(primero, primero + LONGITUDES_BARCOS)]
                 self.casillas = {Case.instances[l + c]
-                              for l, c in product(repeat(letra, longitud), cifras)}
+                              for l, c in product(repeat(letra, LONGITUDES_BARCOS), cifras)}
+                
             else:
                 rang = choice(range(num_columnas))
-                primero = choice(range(num_lineas + 1 - longitud))
+                primero = choice(range(num_lineas + 1 - LONGITUDES_BARCOS))
                 cifra = num2c(rang)
-                letras = [num2l(x) for x in range(primero, primero + longitud)]
+                letras = [num2l(x) for x in range(primero, primero + LONGITUDES_BARCOS)]
                 # Crear el barco
                 self.casillas = {Case.instances[l + c]
-                              for l, c in product(letras, repeat(cifra, longitud))}
+                              for l, c in product(letras, repeat(cifra, LONGITUDES_BARCOS))}
+            return self.casillas
 
-            for existente in instances:
-                if self.casillas.intersection(existente.casillas):
-                    break  # break relativo al "for existente in barcos:"
-            else:
-                # Agregar el barco en el contenedor de barcos
-                instances.append(self)
-                # Informar la casilla que contiene un barco.
-                for casilla in self.casillas:
-                    casilla.barco = self
-                # Agregar estas casillas a las casillas ocupadas :
-                casillas_ocupadas |= self.casillas
-                break  # break relativo al "while True:"
-
+def instanciar(self):
+    for existente in instances:
+        if self.casillas.intersection(existente.casillas):
+            break  # break relativo al "for existente in barcos:"
+        else:
+            # Agregar el barco en el contenedor de barcos
+            instances.append(self)
+            # Informar la casilla que contiene un barco.
+            for casillas in self.casillas:
+                casillas.contiene_barco = True
+            # Agregar estas casillas a las casillas ocupadas :
+            casillas_ocupadas.update(self.casillas)
+                
 @classmethod
-def generar_barcos(self, cls):
+def generar_barcos(cls):
         for longitud in Conventions.barcos_longitud:
-            self.longigudido = longitud
+            cls(longitud, choice(ORIENTACIONES), False, False)
